612bf734d683c2d92529b420b15d866c5a571dbd
KASHI TAMANG <tamang.kashi@gmail.com>
2022-06-28 15:06:30 -0700

    adds pokemon to page from api

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       const data = await getPokemon();
 
       console.log(data);
-      setPokemon(data);
+      setPokemon(data.results);
     }
     loadPokemonData();
   }, []);
@@ -19,18 +19,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          pokemon.map((poke, i) => <div className="pokemon"key={poke.pokemon + i}>
+            <p>{poke.pokemon}</p>
+            <img src={poke.url_image}/>
+          </div>)
+        }
       </header>
     </div>
   );
